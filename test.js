a6d477246da7849257d674cfd8d5168b829baac9
lxq <xiangqian_liu@163.com>
2021-03-16 22:54:28 +0800

    添加一些注釋

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
 /*
  * @Author       : your name
  * @Date         : 2021-03-14 22:25:47
- * @LastEditTime : 2021-03-16 22:35:40
+ * @LastEditTime : 2021-03-16 22:40:20
  * @LastEditors  : Please set LastEditors
  * @Description  : In User Settings Edit
  * @FilePath     : \lagoufed-e-task\test.js
  */
 function curry(fn) {
-  console.log(fn.length)
   return function curryFn(...args) {
-    // args.length: 实参的个数
-    // fn.length: 形参的个数
     if (args.length < fn.length) {
       return function () {
         return curryFn(...args.concat(Array.from(arguments)))
