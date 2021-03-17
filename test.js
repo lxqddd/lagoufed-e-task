2fba524faf853d964f4dbf671dda5d918b6e4160
lxq <xiangqian_liu@163.com>
2021-03-17 22:00:29 +0800

    組合數組

--- test.js
+++ test.js
@@ -1,22 +1,18 @@
-/*
- * @Author       : your name
- * @Date         : 2021-03-14 22:25:47
- * @LastEditTime : 2021-03-16 22:40:20
- * @LastEditors  : Please set LastEditors
- * @Description  : In User Settings Edit
- * @FilePath     : \lagoufed-e-task\test.js
- */
-function curry(fn) {
-  return function curryFn(...args) {
-    if (args.length < fn.length) {
-      return function () {
-        return curryFn(...args.concat(Array.from(arguments)))
-      }
-    }
-    return fn(...args)
+function compose(fn1, fn2) {
+  return function (value) {
+    return fn1(fn2(value))
   }
 }
 
-const currySum = curry((num1, num2, num3) => num1 + num2 + num3)
-console.log(currySum(1, 2, 3))
-console.log(currySum(1)(2)(3))
+const arr = [1, 3, 4, 5, 6]
+
+function reverse(array) {
+  return array.reverse()
+}
+
+function getFirst(array) {
+  return array[0] ? array[0] : null
+}
+
+const composeFn = compose(getFirst, reverse)
+console.log(composeFn(arr))
