8a971a6a39a5e49aa453c037a685bac9bf627afb
lxq <xiangqian_liu@163.com>
2021-03-31 22:07:00 +0800

    模板字符串

--- test.js
+++ test.js
@@ -1,78 +1,12 @@
-/*
- * @Author       : your name
- * @Date         : 2021-03-14 22:25:47
- * @LastEditTime : 2021-03-29 22:23:46
- * @LastEditors  : Please set LastEditors
- * @Description  : In User Settings Edit
- * @FilePath     : \lagoufed-e-task\test.js
- */
-const MyPromise = require('./myPromise.js')
+const name = '夜殇'
+const age = 18
 
-const promise = new MyPromise((resolved, rejected) => {
-  // throw new Error('执行器错误')
-  setTimeout(() => {
-    resolved('success')
-  }, 0)
-  // rejected('fail')
-})
-
-function other() {
-  return new MyPromise((resolve, reject) => {
-    resolve('other')
-  })
+function test(string, name, age) {
+  console.log(string)
+  console.log(arguments)
+  return string[0] + name + string[1] + (age > 18 ? 18 : age) + string[2]
 }
 
-// const p1 = promise.then(value => {
-//   console.log(value)
-//   return p1
-// })
-// p1.then(
-//   value => {
-//     console.log(value)
-//   },
-//   reason => {
-//     console.log(reason.message)
-//   }
-// )
+const str = test`hello ${name}, ${age} years old.`
 
-// promise
-// .then(value => {
-//   console.log(value)
-//   // throw new Error('resolve cuowu ')
-//   return 100
-// })
-// .then()
-// .then(
-//   value => {
-//     console.log('hello world')
-//     console.log(value)
-//   },
-//   reason => {
-//     console.log(reason)
-//   }
-// )
-// promise.then(
-//   value => {
-//     console.log(value)
-//   },
-//   reason => {
-//     console.log(reason)
-//   }
-// )
-
-// MyPromise.resolve(promise).then(value => console.log(value))
-
-function p2() {
-  return new MyPromise((resolve, reject) => {
-    resolve('p2')
-  })
-}
-
-p2()
-  .finally(() => {
-    console.log('hello')
-    return other()
-  })
-  .then(value => {
-    console.log(value)
-  })
+console.log(str)
