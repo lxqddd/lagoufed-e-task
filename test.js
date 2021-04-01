c6441ee3d3f2c549d00e3424b7371acb70b76ee6
lxq <xiangqian_liu@163.com>
2021-04-01 21:59:52 +0800

    Class

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
-// Reflect 提供了一整套操作对象的方法，让我们操作对象的时候更加规范
-const obj = {
-  name: null,
-  age: 18
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  sayHi() {
+    console.log(`hello world, my name is ${this.name}`)
+  }
 }
 
-// 判断当前对象中是否有要找的属性
-console.log(Reflect.has(obj, 'name'))
+const person = new Person('夜殇', 18)
+person.sayHi()
 
-// 删除对象上的某个属性
-Reflect.deleteProperty(obj, 'name')
+class Student extends Person {
+  constructor(name) {
+    super(name)
+  }
+}
 
-// 获取当前对象的所有键
-console.log(Reflect.ownKeys(obj))
-
-console.log(Reflect.getOwnPropertyDescriptor(obj, 'age'))
-
-console.log(obj)
+const student = new Student('yeshang')
+student.sayHi()
