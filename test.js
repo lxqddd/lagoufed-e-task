b591c63a45b1031c62fe31b4fbddaccd20bd063c
lxq <xiangqian_liu@163.com>
2021-04-01 21:33:45 +0800

    proxy代理

--- test.js
+++ test.js
@@ -1,25 +1,33 @@
-const name = '夜殇'
-const age = 18
-
-function test(string, name, age) {
-  console.log(string)
-  console.log(arguments)
-  return string[0] + name + string[1] + (age > 18 ? 18 : age) + string[2]
-}
-
-const str = test`hello ${name}, ${age} years old.`
-
-console.log(str)
-
 const obj = {
   name: '夜殇',
-  // sayHi: function () {
-  //   console.log(this)  // this指向的是当前对象
-  //   console.log(this.name)
-  // },
-  sayHi: () => {
-    console.log(this)
-    console.log(this.name)
-  }
+  age: 18
 }
-obj.sayHi()
+
+const objProxy = new Proxy(obj, {
+  get(target, prop) {
+    // console.log(target, prop)
+    return target[prop] ? target[prop] : '夜殇最帅'
+  },
+  set(target, prop, newValue) {
+    // console.log(target, prop, newValue)
+    if (prop === 'age') {
+      target[prop] = newValue > 18 ? 18 : newValue
+      return
+    }
+    target[prop] = newValue
+  },
+  deleteProperty(target, prop) {
+    if (prop === 'name') {
+      console.log('你丫敢删我试试！！')
+      return
+    }
+    delete target[prop]
+  }
+})
+
+console.log(objProxy.name)
+objProxy.age = 17
+console.log(objProxy.age)
+delete objProxy.name
+delete objProxy.age
+console.log(obj)
