6fa0da13e8b90ad0740066306b3cdfebb9d39d32
lxq <xiangqian_liu@163.com>
2021-05-20 22:37:46 +0800

    webpack 模块二作业

---
+++ part2/fet-e-tsk-02-02/code/mk-vue3/webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const config = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: '3000',
+    publicPath: '/',
+    hot: true
+  },
+  entry: path.resolve(__dirname, './src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: '/node_modules',
+        use: ['babel-loader']
+      },
+      {
+        test: /\.vue$/i,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.(css|less)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      title: 'webpack搭建vue3开发环境'
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin()
+  ]
+}
+
+module.exports = config
