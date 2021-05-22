1afaf4236264e7f909863f3fbbc461de68da0457
lxq <xiangqian_liu@163.com>
2021-05-22 15:35:48 +0800

    feat: 模块二作业

--- part2/fet-e-tsk-02-02/code/mk-vue3/webpack.config.js
+++ part2/fet-e-tsk-02-02/code/mk-vue3/webpack.config.js
@@ -5,34 +5,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const config = {
-  mode: 'development',
+  mode: 'development', // 开发模式
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
     port: '3000',
     publicPath: '/',
     hot: true
   },
-  entry: path.resolve(__dirname, './src/main.js'),
+  entry: path.resolve(__dirname, './src/main.js'), // 打包入口
   output: {
+    // 打包完成后输出的输出内容
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js'
   },
   module: {
     rules: [
       {
+        // js兼容转换
         test: /\.js$/i,
         exclude: '/node_modules',
-        use: ['babel-loader']
+        loader: 'babel-loader'
       },
       {
+        // 处理.vue文件
         test: /\.vue$/i,
         use: 'vue-loader'
       },
       {
+        // 处理css预处理语言less
         test: /\.(css|less)$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
       },
       {
+        // 处理图片
         test: /\.(png|jpg|jpeg|gif)/,
         use: {
           loader: 'url-loader',
