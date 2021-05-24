4ee9de7267994e4bf2c7634c42c857198bde5595
lxq <xiangqian_liu@163.com>
2021-05-24 21:46:31 +0800

    typescript learn

--- typescript-test/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function creatArray(length, value) {
-    var arr = Array(length).fill(value);
-    return arr;
-}
-console.log(creatArray(2, 'hello world'));
-//# sourceMappingURL=index.js.map
