bb59df54b734801923db6affe0900b400d338f47
lxq <xiangqian_liu@163.com>
2021-06-06 21:55:40 +0800

    feat: ts泛型

--- typescript-test/src/index.ts
+++ typescript-test/src/index.ts
@@ -1,6 +1,22 @@
-function creatArray<T>(length: number, value: T): T[] {
-  const arr = Array<T>(length).fill(value)
-  return arr
+// function creatArray<T>(length: number, value: T): T[] {
+//   const arr = Array<T>(length).fill(value)
+//   return arr
+// }
+
+// console.log(creatArray<string>(2, 'hello world'))
+
+function task<T>(arr: T[], num: number): T[] {
+  if (arr.length < num) {
+    return arr
+  }
+  const newArr: T[] = []
+  for (let i = 0; i < num; i++) {
+    newArr.push(arr[i])
+  }
+  return newArr
 }
 
-console.log(creatArray<string>(2, 'hello world'))
+const newArr = task<number>([1, 2, 3, 4], 2)
+console.log(newArr)
+
+type callback<T> = (n: T, i: number) => boolean
