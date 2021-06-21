dcdc38e01e1cf3bdb3d15be4b492c29313818c8d
lxq <xiangqian_liu@163.com>
2021-06-21 22:53:27 +0800

    feat: vue router learn

--- part3/vue-router-learn/src/router/MyVueRouter.js
+++ part3/vue-router-learn/src/router/MyVueRouter.js
@@ -2,16 +2,16 @@ let _Vue = null
 
 class MyVueRouter {
 	static install(Vue) {
-		// 1. 判断当前插件是否已经被加载
+		// 判断当前插件是否已经被安装
 		if (MyVueRouter.install.installed) {
 			return
 		}
 		MyVueRouter.install.installed = true
 
-		// 2. 把Vue构造函数记录到全局变量
+		// 把Vue的构造函数记录到全局变量
 		_Vue = Vue
 
-		// 3. 把创建Vue实例时候传入的router对象注入到Vue实例上
+		// 把创建Vue实例时传入的router对象注入到Vue实例上
 		_Vue.mixin({
 			beforeCreate() {
 				if (this.$options.router) {
@@ -21,30 +21,31 @@ class MyVueRouter {
 			}
 		})
 	}
+
 	constructor(options) {
 		this.options = options
-		this.routeMap = {}
-		this.data = _Vue.observable({
+		this.routerMap = {}
+		this.curRouter = _Vue.observable({
 			current: '/'
 		})
 	}
 
 	init() {
-		this.createRouteMap()
-		this.initComponents(_Vue)
-		this.initEvent()
+		this.createRouterMap()
+		this.createComponent(_Vue)
 	}
 
-	createRouteMap() {
-		// 解析所有的路 由规则，把所有的路由规则解析成键值对的形式，存储到routeMap中
-		if (this.options.routes && this.options.routes.length) {
-			this.options.routes.forEach((item) => {
-				this.routeMap[item.path] = item.component
-			})
+	createRouterMap() {
+		// 遍历所有的路由规则，转换成键值对的形式，存储在routerMap中
+		if (!this.options.routes) {
+			return
 		}
+		this.options.routes.forEach((route) => {
+			this.routerMap[route.path] = route.component
+		})
 	}
 
-	initComponents(Vue) {
+	createComponent(Vue) {
 		Vue.component('router-link', {
 			props: {
 				to: {
@@ -58,38 +59,11 @@ class MyVueRouter {
 					{
 						attrs: {
 							href: this.to
-						},
-						on: {
-							click: this.clickHandler
 						}
 					},
 					[this.$slots.default]
 				)
-			},
-			methods: {
-				clickHandler(e) {
-					history.pushState({}, '', this.to)
-					this.$router.data.current = this.to
-					e.preventDefault()
-				}
 			}
-			// template: `<a :href="to">
-			//               <slot></slot>
-			//             </a>`
-		})
-
-		const self = this
-		Vue.component('router-view', {
-			render(h) {
-				const component = self.routeMap[self.data.current]
-				return h(component)
-			}
-		})
-	}
-
-	initEvent() {
-		window.addEventListener('popstate', () => {
-			this.data.current = window.location.pathname
 		})
 	}
 }
