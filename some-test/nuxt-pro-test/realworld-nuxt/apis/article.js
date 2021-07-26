5160052316b6ab4e852dfe10c51c56928d0a40c8
lxq <xiangqian_liu@163.com>
2021-07-26 22:10:54 +0800

    doc: 补充一些注释信息

--- some-test/nuxt-pro-test/realworld-nuxt/apis/article.js
+++ some-test/nuxt-pro-test/realworld-nuxt/apis/article.js
@@ -1,11 +1,35 @@
 import { fetch } from '../plugins/http'
 
+/**
+ * 获取所有文章标签
+ * @returns
+ */
 export const getTags = () => {
   return fetch.get('/api/tags')
 }
 
+/**
+ * 发布一篇新的文章
+ * @typedef { Article } article
+ * @property { String } body 内容
+ * @property { String } title 标题
+ * @property { String } description 描述内容
+ * @property { Array } tagList 标签列表
+ * @returns
+ */
 export const newArticle = article => {
   return fetch.post('api/articles', {
     article
   })
+}
+
+/**
+ * 获取所有的文章列表
+ * @typedef { ArticleList } params
+ * @property { Number } limit 当前页数
+ * @property { Number } offset 每页多少条
+ * @returns
+ */
+export const getGlobalFeedArticle = params => {
+  return fetch.get(`api/articles?limit=${params.limit}&offset=${params.offset}`)
 }
