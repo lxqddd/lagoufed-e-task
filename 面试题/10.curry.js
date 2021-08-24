feeb738c3eff69f7a6a3206f61095a1585da203c
lxq <xiangqian_liu@163.com>
2021-08-24 22:06:42 +0800

    feat: 函数柯里化

---
+++ 面试题/10.curry.js
@@ -0,0 +1,26 @@
+// 函数柯里化
+
+function add(...args) {
+  return args.reduce((a, b) => a + b)
+}
+
+function currying(fn) {
+  // 其实就是收集所有的参数
+  let args = []
+  return function temp(...newArgs) {
+    if (newArgs.length) {
+      args = [...args, ...newArgs]
+      return temp
+    } else {
+      // 所有最后一次没有传参数，代表要调用该函数了
+      let val = fn.apply(this, args)
+      console.log('hello', val)
+      args = []
+      return val
+    }
+  }
+}
+
+const addCurry = currying(add)
+
+console.log(addCurry(1)(2)(3)(4)(5)())
